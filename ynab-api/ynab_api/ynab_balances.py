d44e76300a0ab2cb9eac5703206afc6fcb92d4e8
Matt Bobke <mcbobke@gmail.com>
2020-04-19 21:56:34 -0700

    Table formatting, API key search in homedir

--- ynab-api/ynab_api/ynab_balances.py
+++ ynab-api/ynab_api/ynab_balances.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
+import pathlib
 import requests
 import sys
+import tabulate
 
 base_url = 'https://api.youneedabudget.com/v1'
+
+def get_apikey() -> str:
+    '''
+    Gets the API key for YNAB, either from an expected file location or from standard input.
+    '''
+    expected_path = pathlib.Path(pathlib.Path.home(), 'ynabkey')
+    if expected_path.exists():
+        with open(expected_path) as ynabkeyfile:
+            api_key = ynabkeyfile.readline()
+    else:
+        api_key = input('Enter your API key: ')
+    return api_key
 
 def get_budgets(headers: dict) -> list:
     '''
@@ -15,25 +29,42 @@ def get_budgets(headers: dict) -> list:
         return response.json()['data']['budgets']
     except requests.HTTPError as err:
         print("An error occurred while requesting your budgets: [{0}]".format(err))
-        return None
+        sys.exit()
 
 def get_accounts(headers: dict, budgets: list) -> list:
     '''
     Gets all accounts under each budget.
     '''
+    accounts = []
     for budget in budgets:
         accounts_url = "{0}/budgets/{1}/accounts".format(base_url, budget['id'])
+        try:
+            response = requests.get(accounts_url, headers=headers)
+            response.raise_for_status()
+            accounts.append(response.json()['data']['accounts'])
+        except requests.HTTPError as err:
+            print("An error occurred while requesting your accounts: [{0}]".format(err))
+            sys.exit()
     
-    return None
+    return accounts
 
 if __name__ == '__main__':
-    api_key = input('Enter your API key: ')
+    api_key = get_apikey()
+    print()
     headers = {
         'Authorization' : "Bearer {0}".format(api_key)
     }
 
-    budgets = get_budgets(headers)
-    if budgets is None:
-        sys.exit()
+    allbudgets = get_budgets(headers)
+    allaccounts = get_accounts(headers, allbudgets)
 
-    accounts = get_accounts(headers, budgets)
+    count = 0
+    while count < len(allbudgets):
+        accountdata = []
+        for account in allaccounts[count]:
+            balance = account['balance'] / 1000
+            accountdata.append((account['name'], balance))
+        print(allbudgets[count]['name'])
+        print(tabulate.tabulate(accountdata, headers=['Account Name', 'Balance'], tablefmt='pretty'))
+        print()
+        count += 1
